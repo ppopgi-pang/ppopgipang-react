d0b802d20bc6f96d0e631e9e710dd3dcf8a31ef5
조하은 <jhe93@naver.com>
2026-02-13 20:12:51 +0900

    add: 아이콘 파일 추가

--- src/assets/icons/common/index.ts
+++ src/assets/icons/common/index.ts
@@ -1,1 +1,4 @@
 export { default as SearchIcon } from './ic-search.svg?react';
+export { default as LeftArrowIcon } from './ic-left-arrow.svg?react';
+export { default as ClockIcon } from './ic-clock.svg?react';
+export { default as CloseIcon } from './ic-x.svg?react';
--- src/assets/icons/map/floating-btn/index.ts
+++ src/assets/icons/map/floating-btn/index.ts
@@ -1,3 +1,4 @@
 export { default as FilterIcon } from './ic-filter.svg?react';
 export { default as LocationIcon } from './ic-location.svg?react';
 export { default as ListIcon } from './ic-menu.svg?react';
+export { default as ReloadIcon } from './ic-reload.svg?react';
