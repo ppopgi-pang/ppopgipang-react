a96f2738d2b4c9ccd1e6450e8f49e4875824909f
조하은 <jhe93@naver.com>
2026-02-15 02:28:31 +0900

    fix: marker 클릭 가능하도록 수정

--- src/components/map/markers/default-marker.tsx
+++ src/components/map/markers/default-marker.tsx
@@ -2,9 +2,10 @@ import { MapMarker } from 'react-kakao-maps-sdk';
 import defaultPositionMarkerIcon from '@/assets/icons/map/markers/ic-default-marker.svg';
 import type { MarkerProps } from '@/types/map/map.types';
 
-export default function DefaultMarker({ position, size = { width: 21, height: 31.5 } }: MarkerProps) {
+export default function DefaultMarker({ position, size = { width: 21, height: 31.5 }, onClick }: MarkerProps) {
     return (
         <MapMarker
+            onClick={onClick}
             position={position}
             image={{
                 src: defaultPositionMarkerIcon,
