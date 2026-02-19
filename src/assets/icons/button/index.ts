4228dc64c8b493a6c5ad0175c2688e37d2ce2cbd
조하은 <jhe93@naver.com>
2026-02-20 02:23:40 +0900

    add: 북마크 이미지 추가

--- src/assets/icons/button/index.ts
+++ src/assets/icons/button/index.ts
@@ -1,2 +1,3 @@
 export { default as EditIcon } from './ic-edit.svg?react';
 export { default as KakaoIcon } from './ic-kakao.svg?react';
+export { default as BookmarkIcon } from './ic-bookmark.svg?react';
